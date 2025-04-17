c179d2ef0f7cc5b8979f3364c4965c2bb727b9eb
Farida Nim <faridanimale@gmail.com>
2025-04-17 14:07:15 +0100

    chapter 3 url mapping

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rango import views
+from django.urls import include
+
 
 urlpatterns = [
+    path('', views.index, name='index'),  # URL pattern for the index view
     path('admin/', admin.site.urls),
+    path('rango/', include('rango.urls')),  # URL patterns for the rango app
 ]
