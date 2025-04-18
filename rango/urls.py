0cd67885acb510d8769d3c6bcc09e14ae5e16bcd
Farida Nim <faridanimale@gmail.com>
2025-04-18 10:53:07 +0100

    chapter 4 with ex

--- rango/urls.py
+++ rango/urls.py
@@ -6,4 +6,4 @@ app_name = 'rango'
 urlpatterns = [
     path('', views.index, name='index'),  # URL pattern for the index view
     path('about/', views.about, name='about'),  # URL pattern for the about view
-]
+] 
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from rango import views
 from django.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('', views.index, name='index'),  # URL pattern for the index view
     path('admin/', admin.site.urls),
     path('rango/', include('rango.urls')),  # URL patterns for the rango app
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
