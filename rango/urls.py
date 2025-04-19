d20f7098d00e12f71cb5d38281ddba97e7de45a5
Farida Nim <faridanimale@gmail.com>
2025-04-19 18:31:22 +0100

    chapter 6 owrking with models and databses and category.html and using filter to filter the pages

--- rango/urls.py
+++ rango/urls.py
@@ -6,4 +6,6 @@ app_name = 'rango'
 urlpatterns = [
     path('', views.index, name='index'),  # URL pattern for the index view
     path('about/', views.about, name='about'),  # URL pattern for the about view
+    path('category/<slug:category_name_slug>/',
+         views.show_category, name = 'show_category'),
 ] 
